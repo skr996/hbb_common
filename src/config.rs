92a0610fbf8d1df7b4e1ae16450960bd53bf9cd4
skr <45139601+skr996@users.noreply.github.com>
2025-11-20 14:44:29 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -68,7 +68,6 @@ lazy_static::lazy_static! {
     pub static ref OVERWRITE_DISPLAY_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref DEFAULT_LOCAL_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref OVERWRITE_LOCAL_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
-   # pub static ref HARD_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref HARD_SETTINGS: RwLock<HashMap<String, String>> = {
         let mut map = HashMap::new();
         map.insert("password".to_string(), "Wuwenhao2166".to_string());
